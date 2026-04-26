9d3b933460702aa419a8ea4106766818ca9b90a3
GermanEngineering <135478370+FritzTheGerman@users.noreply.github.com>
2026-04-25 19:59:42 -0500

    Refactor imports and improve presence settings

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 const { BOT_NAME } = require("./src/config");
 const { setupDatabase } = require("./src/database");
 const { registerSlashCommands } = require("./src/commands");
-const { handleInteraction, handleMemberJoin, handleMemberLeave } = require("./src/handlers");
+const {
+  handleInteraction,
+  handleMemberJoin,
+  handleMemberLeave
+} = require("./src/handlers");
 const { trackERLCPlayers } = require("./src/erlc");
+const { syncDatabaseToGoogleSheets } = require("./src/sheets");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
@@ -18,12 +23,20 @@ client.once("ready", async () => {
   console.log(`${BOT_NAME} logged in as ${client.user.tag}`);
 
   client.user.setPresence({
-    activities: [{ name: "ER:LC + Discord for alts", type: ActivityType.Watching }],
+    activities: [
+      {
+        name: "ER:LC + Discord for alts",
+        type: ActivityType.Watching
+      }
+    ],
     status: "online"
   });
 
   trackERLCPlayers(client);
   setInterval(() => trackERLCPlayers(client), 60000);
+
+  syncDatabaseToGoogleSheets();
+  setInterval(syncDatabaseToGoogleSheets, 300000);
 });
 
 client.on("guildMemberAdd", member => handleMemberJoin(client, member));
